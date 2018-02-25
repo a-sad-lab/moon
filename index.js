690d5c4ce9ff78ea9fa492abc3eaf82a49ba3b03
sadhu <rong8296@gmail.com>
2018-02-25 11:11:59 +0800

    output date in month

---
+++ index.js
@@ -0,0 +1,42 @@
+function getMonth({y, m} = {}) {
+  let moment = require('moment');
+
+  // moment.locale('en'); // week start with monday
+  moment.locale('zh-cn'); // week start with sunday
+  
+  let curY;
+  let curM;
+  
+  if(y && m) {
+    curY = y;
+    curM = m;
+  }else {
+    curY = new Date().getFullYear();
+    curM = new Date().getMonth();
+  }
+
+  let ct = moment([curY, curM]);
+
+  let walk = ct.clone().startOf('week');
+  let end = ct.clone().endOf('month').endOf('week');
+
+  let out = [];
+  while(walk.isSameOrBefore(end, 'date')) {
+    // console.log(walk);
+    let tmp = walk.clone();
+    out.push({
+      moment: tmp,
+      year: tmp.year(),
+      month: tmp.month(),
+      formatedMonth: tmp.month() + 1,
+      date: tmp.date(),
+      day: tmp.day(),
+      available: walk.isSame(ct, 'month')
+    });
+    walk.add(1, 'day');
+  }
+  return out;
+  // console.log(out);
+}
+// console.log(getMonth());
+// console.log(getMonth({y: 2018, m: 3}));
